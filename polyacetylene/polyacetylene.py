e5892ca29c325fcc06d7e27d670a3ced93027245
Germain Salvato Vallverdu <germain.vallverdu@gmail.com>
2026-05-11 22:40:55 +0200

    Add footer signature

--- polyacetylene/polyacetylene.py
+++ polyacetylene/polyacetylene.py
@@ -69,7 +69,7 @@ factor_beta = st.sidebar.slider(
 #     format="%.2f",
 # )
 
-st.sidebar.markdown("$\\alpha_2 = \\alpha_1 + \delta$ (eV)")
+st.sidebar.markdown("$\\alpha_2 = \\alpha_1 + \\delta$ (eV)")
 shift_alpha = st.sidebar.slider(
     "Shift energy",
     min_value=0.0, max_value=1.0, value=0.0, step=0.05,
@@ -253,7 +253,42 @@ $$
 2\beta_1\beta_2\cos(2\pi k)
 }
 $$
-
-
-
 """)
+
+st.markdown("---")
+st.markdown(
+    """
+    <div style="text-align: center; color: gray; font-size: 0.85em; padding: 10px;">
+        <strong>Germain Salvato Vallverdu</strong> &nbsp;|&nbsp;
+        
+        <a href="https://github.com/gVallverdu" style="color: gray;">
+            gVallverdu
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("---")
+st.markdown(
+    """
+    <div style="text-align: center; color: gray; font-size: 0.85em; padding: 10px;">
+        <strong>Germain Salvato Vallverdu</strong> &nbsp;|&nbsp;
+        <a href="https://www.univ-pau.fr" target="_blank" style="color: gray; text-decoration: none;">Université de Pau et des Pays de l'Adour</a> &nbsp;|&nbsp;
+        <a href="https://github.com/gVallverdu" target="_blank" style="color: gray; text-decoration: none;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38
+                         0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13
+                         -.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66
+                         .07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15
+                         -.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27
+                         .68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12
+                         .51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48
+                         0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+            </svg>
+            &nbsp;gVallverdu
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
